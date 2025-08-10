43d942204700c43616e787c2d0706373dced44a5
Yaroslav Yanovich <arikarik782@gmail.com>
2025-08-10 17:53:56 +0300

    feat: add routes and auth headers to shared constants

---
+++ shared-constants/authHeaders.go
@@ -0,0 +1,6 @@
+package sharedconstants
+
+const (
+	HEADER_USER_ID  = "X-User-ID"
+	HEADER_IS_ADMIN = "X-Is-Admin"
+)
---
+++ shared-constants/routes.go
@@ -0,0 +1,9 @@
+package sharedconstants
+
+const (
+	SIGN_UP_ROUTE = "/sign-up"
+	SIGN_IN_ROUTE = "/sign-in"
+	ME_ROUTE      = "/me"
+)
+
+var PRIVATE_ROUTES = []string{ME_ROUTE}
