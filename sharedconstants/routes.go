75119260684b598cf58a6d84f6defa4e8b485229
Yaroslav Yanovich <arikarik782@gmail.com>
2025-08-11 13:54:39 +0300

    feat: add more routes for catalog-service

--- sharedconstants/routes.go
+++ sharedconstants/routes.go
@@ -4,6 +4,10 @@ const (
 	SIGN_UP_ROUTE = "/sign-up"
 	SIGN_IN_ROUTE = "/sign-in"
 	ME_ROUTE      = "/me"
+
+	CATALOG_ROUTE    = "/catalog"
+	CATEGORIES_ROUTE = "/categories"
+	PREVIEW_ROUTE    = "/preview"
 )
 
 var PRIVATE_ROUTES = []string{ME_ROUTE}
