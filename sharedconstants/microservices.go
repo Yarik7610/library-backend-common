b60dfbf0b5e3ebc896a904a2bb3826f24584d669
Yaroslav Yanovich <arikarik782@gmail.com>
2025-08-25 17:54:33 +0300

    refactor: move microservices socket constants from api-gateway to this repo

---
+++ sharedconstants/microservices.go
@@ -0,0 +1,7 @@
+package sharedconstants
+
+const (
+	USER_MICROSERVICE_SOCKET          = "http://user-service:8081"
+	CATALOG_MICROSERVICE_SOCKET       = "http://catalog-service:8082"
+	SUBSCRIPTIONS_MICROSERVICE_SOCKET = "http://subscription-service:8083"
+)
