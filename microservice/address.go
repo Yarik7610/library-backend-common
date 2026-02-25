8ad6b90aec9ca4461eb7a46362cbb7362652364b
Yaroslav Yanovich <arikarik782@gmail.com>
2026-02-25 15:37:23 +0300

    feat: add microservice addresses for gRPC servers

--- microservice/address.go
+++ microservice/address.go
@@ -1,7 +1,12 @@
 package microservice
 
 const (
-	USER_ADDRESS          = "http://user-service:8081"
-	CATALOG_ADDRESS       = "http://catalog-service:8082"
-	SUBSCRIPTIONS_ADDRESS = "http://subscription-service:8083"
+	USER_HTTP_ADDRESS = "http://user-service:8081"
+	USER_GRPC_ADDRESS = "http://user-service:9081"
+
+	CATALOG_HTTP_ADDRESS = "http://catalog-service:8082"
+	CATALOG_GRPC_ADDRESS = "http://catalog-service:9082"
+
+	SUBSCRIPTIONS_HTTP_ADDRESS = "http://subscription-service:8083"
+	SUBSCRIPTIONS_GRPC_ADDRESS = "http://subscription-service:9083"
 )
