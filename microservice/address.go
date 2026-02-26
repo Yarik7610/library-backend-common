09d2f56c1096b87c4350ac0d02578a508cd4faa8
Yaroslav Yanovich <arikarik782@gmail.com>
2026-02-26 15:46:49 +0300

    fix: update microservice gRPC addresses format

--- microservice/address.go
+++ microservice/address.go
@@ -2,11 +2,11 @@ package microservice
 
 const (
 	USER_HTTP_ADDRESS = "http://user-service:8081"
-	USER_GRPC_ADDRESS = "http://user-service:9081"
+	USER_GRPC_ADDRESS = "user-service:9081"
 
 	CATALOG_HTTP_ADDRESS = "http://catalog-service:8082"
-	CATALOG_GRPC_ADDRESS = "http://catalog-service:9082"
+	CATALOG_GRPC_ADDRESS = "catalog-service:9082"
 
 	SUBSCRIPTIONS_HTTP_ADDRESS = "http://subscription-service:8083"
-	SUBSCRIPTIONS_GRPC_ADDRESS = "http://subscription-service:9083"
+	SUBSCRIPTIONS_GRPC_ADDRESS = "subscription-service:9083"
 )
